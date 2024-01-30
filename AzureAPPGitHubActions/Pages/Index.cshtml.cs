ba6381b1ad8e64896a71dd738d27f086ac4cec66
stapemmasani <stapemmasani@gmail.com>
2024-01-30 15:33:56 +0530

    Application isight mechanism added

--- AzureAPPGitHubActions/Pages/Index.cshtml.cs
+++ AzureAPPGitHubActions/Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AzureAPPGitHubActions.Pages
@@ -6,15 +5,17 @@ namespace AzureAPPGitHubActions.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greetings"] = _configuration["Greetings"];
         }
     }
 }
